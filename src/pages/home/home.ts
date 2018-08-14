55501eb0c03251e1bbe641995a38cd857b53477a
unknown <mokoenatlp@gmail.com>
2018-08-14 09:58:45 +0200

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {SignupPage} from '../../pages/signup/signup';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,7 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
-
+send(){
+  this.navCtrl.push(SignupPage);
+}
 }
